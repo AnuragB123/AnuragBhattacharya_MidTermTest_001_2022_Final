bd75b4d6e20c9b4d28bdbfe0ae45dcfbe29dcddf
AnuragB123 <abhatt66@my.centennialcollege.ca>
2022-03-04 09:40:51 -0500

    Database Index Documentation

--- Anurag_Midterm_Test/server/db/index.js
+++ Anurag_Midterm_Test/server/db/index.js
@@ -1,11 +1,14 @@
+//Import necessary modules
 const mongoose = require('mongoose')
 
+//Connection to the Mongoose database
 mongoose
     .connect('mongodb://127.0.0.1:27017/AnuragBhattacharya-appointment-db', { useNewUrlParser: true })
     .catch(e => {
         console.error('Connection error', e.message)
     })
 
+//Storing the Connection
 const db = mongoose.connection
-
+//Exporting the Connection
 module.exports = db
