02e7383b9b9cc250ae61a2fa56c3f88396a51eeb
AnuragB123 <abhatt66@my.centennialcollege.ca>
2022-03-03 20:31:50 -0500

    APIS updated

--- Anurag_Midterm_Test/client/src/api/index.js
+++ Anurag_Midterm_Test/client/src/api/index.js
@@ -4,18 +4,14 @@ const api = axios.create({
     baseURL: 'http://localhost:3000/api',
 })
 
-export const insertMovie = payload => api.post(`/movie`, payload)
-export const getAllMovies = () => api.get(`/movies`)
-export const updateMovieById = (id, payload) => api.put(`/movie/${id}`, payload)
-export const deleteMovieById = id => api.delete(`/movie/${id}`)
-export const getMovieById = id => api.get(`/movie/${id}`)
+export const insertAppointment = payload => api.post(`/appointment`, payload)
+export const getAllAppointments = () => api.get(`/appointments`)
+export const deleteAppointmentById = id => api.delete(`/appointment/${id}`)
 
 const apis = {
-    insertMovie,
-    getAllMovies,
-    updateMovieById,
-    deleteMovieById,
-    getMovieById,
+    insertAppointment,
+    getAllAppointments,
+    deleteAppointmentById,
 }
 
 export default apis
