61339fcc00c4e4dfecdf1c33ccd16b5f8967cc1a
AnuragB123 <abhatt66@my.centennialcollege.ca>
2022-03-04 09:52:00 -0500

    Appointment Router Documentation

--- Anurag_Midterm_Test/server/routes/appointment-router.js
+++ Anurag_Midterm_Test/server/routes/appointment-router.js
@@ -1,11 +1,11 @@
+//Import necessary modules
 const express = require('express')
-
 const AppointmentCtrl = require('../controllers/appointment-ctrl')
-
+//Router for the CRUD operations
 const router = express.Router()
-
+//POST, DELETE and GET Operations
 router.post('/appointment', AppointmentCtrl.createAppointment)
 router.delete('/appointment/:id', AppointmentCtrl.deleteAppointment)
 router.get('/appointments', AppointmentCtrl.getAppointments)
-
+//Exports the Router Function
 module.exports = router
