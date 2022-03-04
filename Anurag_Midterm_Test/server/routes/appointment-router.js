eee9fa96edbe6e146d788a5523f8a47d2d53fa26
AnuragB123 <abhatt66@my.centennialcollege.ca>
2022-03-03 20:25:41 -0500

    Router Fixed

--- Anurag_Midterm_Test/server/routes/appointment-router.js
+++ Anurag_Midterm_Test/server/routes/appointment-router.js
@@ -1,13 +1,11 @@
 const express = require('express')
 
-const MovieCtrl = require('../controllers/movie-ctrl')
+const AppointmentCtrl = require('../controllers/appointment-ctrl')
 
 const router = express.Router()
 
-router.post('/movie', MovieCtrl.createMovie)
-router.put('/movie/:id', MovieCtrl.updateMovie)
-router.delete('/movie/:id', MovieCtrl.deleteMovie)
-router.get('/movie/:id', MovieCtrl.getMovieById)
-router.get('/movies', MovieCtrl.getMovies)
+router.post('/appointment', AppointmentCtrl.createAppointment)
+router.delete('/appointment/:id', AppointmentCtrl.deleteAppointment)
+router.get('/appointments', AppointmentCtrl.getAppointments)
 
 module.exports = router
