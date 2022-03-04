e33a1fe9b03cf886541e52c64649bbd32462728f
AnuragB123 <abhatt66@my.centennialcollege.ca>
2022-03-04 09:57:43 -0500

    API Index Documentation

--- Anurag_Midterm_Test/client/src/api/index.js
+++ Anurag_Midterm_Test/client/src/api/index.js
@@ -1,17 +1,20 @@
+//Import necessary modules
 import axios from 'axios'
-
+//API Url
 const api = axios.create({
     baseURL: 'http://localhost:3000/api',
 })
 
+//Export the Neccessary Functions
 export const insertAppointment = payload => api.post(`/appointment`, payload)
 export const getAllAppointments = () => api.get(`/appointments`)
 export const deleteAppointmentById = id => api.delete(`/appointment/${id}`)
 
+//APIs needed for the React Client
 const apis = {
     insertAppointment,
     getAllAppointments,
     deleteAppointmentById,
 }
-
+//Export All API Functions
 export default apis
