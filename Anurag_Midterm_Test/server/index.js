2d07459f8d4e813ed5035c9c645e294a683c63d5
AnuragB123 <abhatt66@my.centennialcollege.ca>
2022-03-04 09:55:17 -0500

    Index Documentation

--- Anurag_Midterm_Test/server/index.js
+++ Anurag_Midterm_Test/server/index.js
@@ -1,23 +1,22 @@
+//Import necessary modules
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
 const db = require('./db')
 const appointmentRouter = require('./routes/appointment-router')
-
 const app = express()
 const apiPort = 3000
-
+//Different packages the server application will use
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
-
+//MongoDB Connection Error
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
+//The Server is Running
 app.get('/', (req, res) => {
     res.send('Server is Running')
 })
-
+//Getting the Appointment
 app.use('/api', appointmentRouter)
-
+//Listening to the API Port specified
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
